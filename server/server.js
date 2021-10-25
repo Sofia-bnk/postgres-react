b2de5c6881e7b0bef1db29e3ecbecfb5220dd7b1
Sofia Bonakdar <sofia@bonakdar.se>
2021-10-25 19:50:05 +0200

    get/post

--- server/server.js
+++ server/server.js
@@ -6,18 +6,38 @@ const db = require("./db");
 app.use(express.json());
 
 app.get("/tasks", async (req, res) => {
-  const result = await db.query("select * from tasks");
+  try {
+    const result = await db.query("select * from tasks");
 
-  console.log(result);
-  res.status(200).json({ status: "success", task: "clean" });
+    res.status(200).json({ data: { tasks: result.rows } });
+  } catch (err) {
+    err;
+  }
 });
 
-app.get("/tasks/:id", (req, res) => {
-  res.status(200).send(req.params.id);
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const result = await db.query("select * from tasks where id = $1", [
+      req.params.id,
+    ]);
+
+    res.status(200).json({ data: { task: result.rows[0] } });
+  } catch (err) {
+    err;
+  }
 });
 
-app.post("/tasks", (req, res) => {
-  res.status(201).send(req.body);
+app.post("/tasks", async (req, res) => {
+  try {
+    const result = await db.query(
+      "insert into tasks(task_date,day,task) values ($1,$2,$3) returning *", // returning * for sending back the input
+      [req.body.task_date, req.body.day, req.body.task]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.put("/tasks/:id", (req, res) => {
