ac9216ba80363f81d9e7e2c7e34fa5987ceffa4b
Sofia Bonakdar <sofia@bonakdar.se>
2021-10-26 13:01:55 +0200

    added Client file

--- server/server.js
+++ server/server.js
@@ -40,11 +40,25 @@ app.post("/tasks", async (req, res) => {
   }
 });
 
-app.put("/tasks/:id", (req, res) => {
-  res.status(200).json({ status: "success", task: "tidy" });
+app.put("/tasks/:id", async (req, res) => {
+  try {
+    const result = await db.query(
+      "update tasks set task_date=$1, day=$2, task=$3 where id=$4 returning *",
+      [req.body.task_date, req.body.day, req.body.task, req.params.id]
+    );
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
 });
-app.delete("/tasks/:id", (req, res) => {
-  res.status(204).res(req.params.id);
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    await db.query("delete from tasks where id=$1", [req.params.id]);
+
+    res.status(204).send(`taske ${req.params.id} is deleted`);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 const port = process.env.PORT || 3001;
